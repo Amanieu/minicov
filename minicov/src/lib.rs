a4c0f8854de0bb356423d88380cdbaee38e287b0
Amanieu d'Antras <amanieu@gmail.com>
2022-09-13 16:25:07 +0800

    Fix rustfmt

--- minicov/src/lib.rs
+++ minicov/src/lib.rs
@@ -13,7 +13,7 @@
 //! Note that to profile both C and Rust code at the same time you must use Clang
 //! with the same LLVM version as the LLVM used by rustc. You can pass these flags
 //! to C code compiled with the `cc` crates using [environment variables].
-//! 
+//!
 //! [environment variables]: https://github.com/rust-lang/cc-rs#external-configuration-via-environment-variables
 //!
 //! ## Usage
